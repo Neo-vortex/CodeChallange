ccda30d1cab15b996c9ba4d939a029ece809503e
Mohamadreza Nakhleh <neo.vortex@pm.me>
2022-07-07 17:24:31 +0430

    Add unit test and DI container

--- src/CodeChallange/Program.cs
+++ src/CodeChallange/Program.cs
@@ -1,12 +1,44 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Serilog;
+
+
+#region WebhostBuilding
+// This is the main entry point of the application.
+// Using the default ASP.NET Core configuration.
 var builder = WebApplication.CreateBuilder(args);
+#endregion
+#region ConfigureLogging
+// Configure logging.
+// Log to the console and to a file.
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .WriteTo.Console()
+    .Enrich.FromLogContext()
+    .CreateLogger();
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
+#endregion
+#region Services
+// Using Autofac, we can create a container that can be used to resolve dependencies.
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 
-// Add services to the container.
+// Register the Autofac services in the container.
+builder.Host.ConfigureContainer<ContainerBuilder>(builder =>
+{
 
+    /*builder.RegisterType<AppLogger>().As<IAppLogger>().SingleInstance();
+    builder.RegisterType<DataAccess>().As<IDataAccess>().SingleInstance();*/
+});
+#endregion
+#region etc
+// Add Controllers to the pipeline.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+// Add the Swagger generator to the services
 builder.Services.AddSwaggerGen();
-
+#endregion
+#region Run
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,10 +48,17 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
 
-app.UseAuthorization();
+// use https if needed
+/*app.UseHttpsRedirection();*/
 
+// use authorization if needed
+/*app.UseAuthorization();*/
+
+// Add the routing middleware to the request pipeline.
 app.MapControllers();
 
+// Start the application
 app.Run();
+#endregion
+
